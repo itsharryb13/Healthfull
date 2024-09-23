4ffad54000d1bdb2369ffc947473268b2b810f91
ekim8484 <143744364+ekim8484@users.noreply.github.com>
2024-09-23 20:39:18 +0000

    bug fixes

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -10,9 +10,9 @@ const clientCredentials = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+};
 
-const app = getApps.length > 0 ? getApp() : initializeApp(clientCredentials)
-const auth = getAuth(app)
+const app = getApps.length > 0 ? getApp() : initializeApp(clientCredentials);
+const auth = getAuth(app);
 
-export {app, auth}
+export {auth};
